0fe719ebf2dad3f2bc2349a8c1accbaa6f3751ca
Noa Cohen <noac@moveo.co.il>
2022-06-28 11:52:14 +0300

    test print

--- Sources/Starscream/WebSocket.swift
+++ Sources/Starscream/WebSocket.swift
@@ -117,6 +117,7 @@ open class WebSocket: WebSocketClient, EngineDelegate {
     
     public convenience init(request: URLRequest, certPinner: CertificatePinning? = FoundationSecurity(), compressionHandler: CompressionHandler? = nil, useCustomEngine: Bool = true) {
         if #available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *), !useCustomEngine {
+                debugPrint("Noa")
             self.init(request: request, engine: NativeEngine())
         } else if #available(macOS 10.14, iOS 12.0, watchOS 5.0, tvOS 12.0, *) {
             self.init(request: request, engine: WSEngine(transport: TCPTransport(), certPinner: certPinner, compressionHandler: compressionHandler))
